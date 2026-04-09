8da47c31577aaf35be8f786f1727d6215fc351be
diegobfcy <diegobfcy@gmail.com>
2026-04-09 18:02:08 -0400

    last

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/mi-proyecto',
-  assetPrefix: '/mi-proyecto/',
   images: {
     unoptimized: true
-  }
+  },
+  basePath: '/hemai',       // nombre de tu repo
+  assetPrefix: '/hemai/',
 };
 
 export default nextConfig;
